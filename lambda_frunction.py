4243fb76955307fc1dcc8ed9fc73155bc6687b1b
Garvit-Jain-2010 <garvitrajjain34@gmail.com>
2025-04-21 16:42:34 +0530

    garvit

--- lambda_frunction.py
+++ lambda_frunction.py
@@ -14,6 +14,7 @@
 # si= lambda p,r,t : (p*r*t)/100
 # print(si(1000,3,4))
 
-students = [('John', 90), ('Jane', 85), ('Dave', 95)]
-students.sort(key=lambda x: x[1])
-print(students)  # Output: [('Jane', 85), ('John', 90), ('Dave', 95)]
+# students = [('John', 90), ('Jane', 85), ('Dave', 95)]
+# students.sort(key=lambda x: x[1])
+#  print(students)  # Output: [('Jane', 85), ('John', 90), ('Dave', 95)]
+
