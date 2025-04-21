ae9b2db261f6fbf11f29dfb9343edc7eca64da58
Garvit-Jain-2010 <garvitrajjain34@gmail.com>
2025-04-21 16:31:22 +0530

    garvit

---
+++ main.py
@@ -0,0 +1,19 @@
+# square = lambda x:x*x
+# print(square(25))
+
+# x = lambda x : x % 2 == 0
+# print(x(10))
+
+# def si(p, t, r):
+#     print(f"SI: {(p*r*t)/100}.")
+# a = int(input("Enter the principal: "))
+# b = int(input("Enter the rate of interest: "))
+# c = int(input("Enter the time period: "))
+# si(a, b, c)
+
+# si= lambda p,r,t : (p*r*t)/100
+# print(si(1000,3,4))
+
+students = [('John', 90), ('Jane', 85), ('Dave', 95)]
+students.sort(key=lambda x: x[1])
+print(students)  # Output: [('Jane', 85), ('John', 90), ('Dave', 95)]
