d62a065ce962bb7e98e7503d3f30da8fcc1b1456
Garvit-Jain-2010 <garvitrajjain34@gmail.com>
2025-04-23 17:42:51 +0530

    garvit

---
+++ map_function.py
@@ -0,0 +1,23 @@
+# Square the numbers in list.
+# def square(x):
+#     return x * x
+# numbers = [1, 2, 3, 4, 5]
+# squared = map(square, numbers)
+# squared_list = list(squared)
+# print(squared_list)
+
+# Convert all strings to uppercase.
+# def uppercase(y):
+#     return y.upper()
+# name = ["Garvit", "Shobhit", "Raj"]
+# captial = map(uppercase, name)
+# c = list(captial)
+# print(c)
+
+# Add 5 to each number.
+# def add(z):
+#     return z + 5
+# num = [4, 7, 3, 9, 5]
+# add_5 = map(add, num)
+# a = list(add_5)
+# print(a)
