dc18f9cc3b2693bd577127fd2289b6c87d87b0d2
Garvit-Jain-2010 <garvitrajjain34@gmail.com>
2025-04-28 16:51:14 +0530

    garvit

--- Dictionary/tempCodeRunnerFile.py
+++
@@ -1,2 +0,0 @@
-marks = x["midterm_marks"][0]
-# print(marks)
---
+++ Tests/question.py
@@ -0,0 +1,23 @@
+x = {
+    "student":["Garvit", "Shyam", "Mohit", "Geeta", "Shobhit", "Ravi", "Shivam", "Saurabh", "Amit", "Piyush"],
+    "current_sem": [1, 3, 2, 4, 3, 2, 1, 4, 2, 4],
+    "attendance": [56, 78, 90, 45, 67, 89, 34, 45, 78, 90],
+    "previous_sem_marks": [45, 67, 89, 23, 88, 76, 90, 45, 67, 80],
+    "previous_sem_attendance":[67, 78, 90, 45, 80, 85, 90, 70, 75, 80],
+    "presentation":[3, 2, 6 , 7, 5, 4, 3, 2, 1, 0],
+    "midterm_marks":[
+        {"python": 85, "java": 90, "c++": 80, "web-development": 89},
+        {"python": 78, "java": 88, "c++": 75, "web-development": 80},
+        {"python": 92, "java": 80, "c++": 85, "web-development": 87},
+        {"python": 70, "java": 75, "c++": 65, "web-development": 72},
+        {"python": 88, "java": 85, "c++": 78, "web-development": 84},
+        {"python": 76, "java": 80, "c++": 70, "web-development": 75},
+        {"python": 90, "java": 95, "c++": 88, "web-development": 92},
+        {"python": 45, "java": 50, "c++": 40, "web-development": 48},
+        {"python": 67, "java": 70, "c++": 60, "web-development": 65},
+        {"python": 80, "java": 85, "c++": 75, "web-development": 82}
+    ],  
+    "gender":["male", "male", "male", "female", "female", "female", "male", "male", "female", "male"],
+    "pass":["yes", "no", "no", "yes", "yes", "no", "yes", "no", "yes", "no"],
+    "fail":[0, 2, 1, 0, 0, 1, 0, 1, 0, 1],
+}
