a999d9db49009bb3a7dd9e2cd60ebb4041b7fab0
Garvit-Jain-2010 <garvitrajjain34@gmail.com>
2025-04-29 17:11:07 +0530

    garvit

---
+++ filter_frunction.py
@@ -0,0 +1,7 @@
+# files = ["doc1.pdf", "notes.txt", "report.docx", "data.txt"]
+# txt_files = list(filter(lambda f: f.endswith(".txt"), files))
+# print("TXT files:", txt_files)  # ['notes.txt', 'data.txt']
+
+# salary = [1330, 2500, 2000, 2001]
+# x = list(filter(lambda c: c>2000, salary))
+# print(x)
