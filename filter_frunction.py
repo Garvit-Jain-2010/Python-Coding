cc7ec88e655e14c66666d9ee0fd5cce5f5ff4c3a
Garvit-Jain-2010 <garvitrajjain34@gmail.com>
2025-04-30 16:59:06 +0530

    garvit

--- filter_frunction.py
+++ filter_frunction.py
@@ -9,3 +9,17 @@
 # names = ["Garvit Jain", "Rohit Sharma", "Kartik Jain", "Aditya Sharma", "Atharv Singh", "Avykt Rana"]
 # end = list(filter(lambda v: v.endswith(" Jain"), names))
 # print(end)
+
+x = {
+    "student":["Garvit", "Shyam", "Mohit", "Geeta", "Shobhit"],
+    "midterm_marks":[
+        {"maths": 85, "science": 90, "hindi": 80, "english": 89},
+        {"maths": 78, "science": 88, "hindi": 75, "english": 80},
+        {"maths": 92, "science": 80, "hindi": 85, "english": 87},
+        {"maths": 70, "science": 75, "hindi": 65, "english": 72},
+        {"maths": 88, "science": 85, "hindi": 78, "english": 84},
+    ],  
+}
+
+# Get the highese marks of each subject with the name of the stufent.
+
