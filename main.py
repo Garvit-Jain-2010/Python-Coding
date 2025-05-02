c207cfdf6e64fdb99d2e53ac3309d7dde6711cd5
Garvit-Jain-2010 <garvitrajjain34@gmail.com>
2025-05-02 17:40:47 +0530

    garvit

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 data = {
-    "name" : ["Elon Musk", "Jeff Bezos"]  
+    "name" : ["Elon Musk", "Jeff Bezos", "Bernard Arnault", "Bill Gates", "Warren Buffett"],
+    "net_worth" : [219000000000, 171000000000, 158000000000, 129000000000, 118000000000],
+    "age" : [50, 58, 73, 66, 91],
+    "country" : ["U.S.", "U.S.", "France", "U.S.", "U.S.",],
+    "source" : ["Tesla", "Amazon", "LVMH", "Microsoft", "Berkshire Hathaway"]
 }
