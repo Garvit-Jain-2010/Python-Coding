4f802302884e5f2c2376780fa9fd6748ff9841e1
Garvit-Jain-2010 <garvitrajjain34@gmail.com>
2025-05-02 17:42:50 +0530

    garvit

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ data = {
     "net_worth" : [219000000000, 171000000000, 158000000000, 129000000000, 118000000000],
     "age" : [50, 58, 73, 66, 91],
     "country" : ["U.S.", "U.S.", "France", "U.S.", "U.S.",],
-    "source" : ["Tesla", "Amazon", "LVMH", "Microsoft", "Berkshire Hathaway"]
+    "source" : ["Tesla", "Amazon", "LVMH", "Microsoft", "Berkshire Hathaway"],
+    "industry" : ["Automotive", "Technology", "Fashion & Retail", "Technology", "Finance & Investments"]
 }
