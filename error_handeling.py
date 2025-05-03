0bc6baf184cfc244c0c79d71eed3393972bbb199
Garvit-Jain-2010 <garvitrajjain34@gmail.com>
2025-05-03 17:15:13 +0530

    garvit

--- error_handeling.py
+++ error_handeling.py
@@ -11,8 +11,26 @@
 # except Exception as e:
 #     print(f"An error occurred: {e}")
 
+# try:
+#     num = int(input("Enter an integer: "))
+#     print(f"Your number is {num}.")
+# except Exception as r:
+#     print("Error:", r)
+
+# try:
+#     x = [23, 45, 67]
+#     index = int(input("Enter index: "))
+#     print("Value:", x[index])
+# except Exception as s:
+#     print(f"Error: {s}")
+
+
+list = [10, 20, 30, 40, 50]
 try:
-    num = int(input("Enter an integer: "))
-    print(f"Your number is {num}.")
+    num = int(input("Enter a number to search: "))
+    if num in list:
+        print(f"{num} is in the list.")
+    else:
+        raise ValueError
 except ValueError:
-    print("Invalid input. Please enter a valid integer.")
+    print(f"{num} is not in the list.")
