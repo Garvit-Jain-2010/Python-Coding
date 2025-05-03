f07b0b19c147c05326cc9efd8cc76422f983bc6c
Garvit-Jain-2010 <garvitrajjain34@gmail.com>
2025-05-03 16:47:04 +0530

    garvit

---
+++ error_handeling.py
@@ -0,0 +1,12 @@
+# try:
+#     a = int("abc")
+# except ValueError:
+#     print("Conversion failed: Not a number.")
+
+# try:
+#     a = int(input("Enter numerator: "))
+#     b = int(input("Enter denominator: "))
+#     result = a / b
+#     print("Result:", result)
+# except Exception as e:
+#     print(f"An error occurred: {e}")
