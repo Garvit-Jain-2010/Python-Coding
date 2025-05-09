c021f45ece0f2f8c4c4463ffb05422eddd2c34fb
Garvit-Jain-2010 <garvitrajjain34@gmail.com>
2025-05-09 17:17:08 +0530

    garvit

---
+++ class.py
@@ -0,0 +1,38 @@
+# 1
+# class person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age   
+
+#     def greet(self):
+#         print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+# p1 = person("Alice", 30)
+# p1.greet()
+
+# 2
+# class student:
+#     def __init__(self, name, age, clas):
+#         self.name = name
+#         self.age = age
+#         self.class_name = clas
+
+#     def greet(self):
+#         print(f"Hello, my name is {self.name} and I am {self.age} years old. I am in class {self.class_name}.")
+    
+# s1 = student("Alice", 30, "10th")
+# s1.greet()
+
+# 3
+# class employee:
+#     def __init__(self, name, age, salary):
+#         self.name = name
+#         self.age = age
+#         self.salary = salary
+
+#     def bonus(self):
+#         # With a 10% bonus
+#         return self.salary * 0.10
+
+# a = employee("Alice", 30, 50000)
+# print(f"Bonus for {a.name}: {a.bonus()}")
