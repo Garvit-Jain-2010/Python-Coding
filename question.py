57fd68e0aefa2d3b4d5f8ce9970654791aa63d03
Garvit-Jain-2010 <garvitrajjain34@gmail.com>
2025-06-07 16:44:11 +0530

    garvit

---
+++ question.py
@@ -0,0 +1,24 @@
+data = {
+    "name" : ["Elon Musk", "Jeff Bezos", "Bernard Arnault", "Bill Gates", "Warren Buffett"],
+    "net_worth" : [219000000000, 171000000000, 158000000000, 129000000000, 118000000000],
+    "age" : [50, 58, 73, 66, 91],
+    "country" : ["U.S.", "U.S.", "France", "U.S.", "U.S.",],
+    "source" : ["Tesla", "Amazon", "LVMH", "Microsoft", "Berkshire Hathaway"],
+    "industry" : ["Automotive", "Technology", "Fashion & Retail", "Technology", "Finance & Investments"]
+}
+
+# def tech_industry(data):
+#     tech_people = []
+#     for i in range(len(data["name"])):
+#         if data["industry"][i] == "Technology":
+#             tech_people.append(data["name"][i])
+#     return tech_people
+
+# for person in tech_industry(data):
+#     print(f"{person}")
+
+# tech_people = []
+# for i in range(len(data["name"])):
+#     if data["industry"][i] == "Technology":
+#         tech_people.append(data["name"][i])
+# print(tech_people)
