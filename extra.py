e8d6d799601360c3c902d8072960c503ceac2077
Garvit-Jain-2010 <garvitrajjain34@gmail.com>
2025-09-04 20:02:24 +0530

    garvit

--- extra.py
+++ extra.py
@@ -1,4 +1,16 @@
-sentence = input("Enter a sentence: ")
-words = sentence.split()
-reversed_sentence = ' '.join(reversed(words))
-print("Reversed sentence:", reversed_sentence)
+# sentence = input("Enter a sentence: ")
+# words = sentence.split()
+# reversed_sentence = ' '.join(reversed(words))
+# print("Reversed sentence:", reversed_sentence)
+
+class Employee:
+    a = 1
+    
+    @classmethod
+    def show(cls):
+        print(f"The class attribute of a is {cls.a}")
+
+e = Employee()
+e.a = 45
+
+e.show()
---
+++ findthesecodnlargestnumber.py
@@ -0,0 +1,3 @@
+numbers = [45, 56, 23, 68, 47, 23]
+numbers.remove(max(numbers))
+print("Second largest number:", max(numbers))
